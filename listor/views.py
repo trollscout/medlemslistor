dc8a51d7a892fd29a2d2162810d478f7c7b81014
Zyberit <hakan@violaberg.nu>
2017-07-26 22:59:05 +0200

    just som comments

--- listor/views.py
+++ listor/views.py
@@ -60,3 +60,35 @@ def telefonlista(memdata):
 
 def allepost(memdata):
     pass
+
+
+
+# contact_home_phone {'value': '087763327'}
+# prev_term_due_date {'value': '2016-10-31'}
+# contact_mobile_mum {'value': '0722333777'}
+# first_name {'value': 'Veronica'}
+# prev_term {'value': 'Betalt', 'raw_value': 'paid'}
+# contact_fathers_name {'value': 'Håkan Persson'}
+# unit {'value': 'Slow Fox', 'raw_value': '8907'}
+# current_term {'value': 'Betalt', 'raw_value': 'paid'}
+# created_at {'value': '2011-11-05'}
+# email {'value': 'veronica@violaberg.nu'}
+# date_of_birth {'value': '2002-01-02'}
+# contact_mobile_dad {'value': '0702178177'}
+# country {'value': 'Sverige'}
+# last_name {'value': 'Ekenberg'}
+# ssno {'value': '20020102-1904'}
+# sex {'value': 'Kvinna', 'raw_value': '2'}
+# member_no {'value': '3230994'}
+# contact_mobile_phone {'value': '0767994483'}
+# address_1 {'value': 'Violabergsvägen 4'}
+# group {'value': 'Trollbäckens scoutkår', 'raw_value': '784'}
+# status {'value': 'Aktiv', 'raw_value': '2'}
+# confirmed_at {'value': '2014-08-20'}
+# current_term_due_date {'value': '2017-03-31'}
+# postcode {'value': '13668'}
+# contact_email_mum {'value': 'eva@violaberg.nu'}
+# contact_mothers_name {'value': 'Eva Ekenberg'}
+# contact_email_dad {'value': 'hakan@violaberg.nu'}
+# town {'value': 'Vendelsö'}
+
