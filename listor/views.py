a31799b9a8de10120ae1f27d88f3f0a595070d03
Zyberit <hakan@violaberg.nu>
2017-07-27 07:57:29 +0200

    First shot at dbx

--- listor/views.py
+++ listor/views.py
@@ -9,7 +9,7 @@ Created on 7 apr. 2017
 from django.http import HttpResponse
 
 from .scoutnet import get_memdata
-
+from .dbx import save_file
 
 def go(request):
     memdata = get_memdata()['data']
@@ -27,30 +27,48 @@ def mk_listor(memdata):
     telefonlista(memdata)
     allepost(memdata)
 
+# def epostlistor(memdata):
+#     def v(m,f):
+#         return memdata[m][f]['value'] if f in memdata[m] else ""
+# 
+#     for avd in avdelningar:
+#         mlist = [m for m in memdata if memdata[m]['unit']['value'] == avd and memdata[m]['date_of_birth']['value'] > "1992-01-01"]
+#         elist = []
+#         for m in mlist:
+#             namn = v(m,'first_name')+" "+v(m,'last_name')
+#             if v(m,'email') != "": 
+#                 elist.append([namn,"<"+v(m,'email')+">"])
+#             if v(m,'contact_email_mum') != "" and v(m,'contact_email_mum') != v(m,'email'):
+#                 elist.append([v(m,'contact_mothers_name')+" ("+namn+"s mamma)","<"+v(m,'contact_email_mum')+">"])
+#             if v(m,'contact_email_dad') != "" and v(m,'contact_email_dad') != v(m,'email'):
+#                 elist.append([v(m,'contact_fathers_name') +" ("+namn+"s pappa)","<"+v(m,'contact_email_dad')+">"])
+#             if v(m,'contact_alt_email') != "":
+#                 elist.append([namn+" (Extra)","<"+v(m,'contact_alt_email')+">"])
+#         f = open("/home/hakan/Temp/listor/"+avd+".txt","w")
+#         for l in elist:
+#             s = l[0]+"  "+l[1]+";\n"
+#             f.write(s)
+#         f.close()
+
 def epostlistor(memdata):
     def v(m,f):
         return memdata[m][f]['value'] if f in memdata[m] else ""
 
     for avd in avdelningar:
         mlist = [m for m in memdata if memdata[m]['unit']['value'] == avd and memdata[m]['date_of_birth']['value'] > "1992-01-01"]
-        elist = []
+        elista = ""
         for m in mlist:
             namn = v(m,'first_name')+" "+v(m,'last_name')
             if v(m,'email') != "": 
-                elist.append([namn,"<"+v(m,'email')+">"])
+                elista += namn+" <"+v(m,'email')+">" 
             if v(m,'contact_email_mum') != "" and v(m,'contact_email_mum') != v(m,'email'):
-                elist.append([v(m,'contact_mothers_name')+" ("+namn+"s mamma)","<"+v(m,'contact_email_mum')+">"])
+                elista += v(m,'contact_mothers_name')+" ("+namn+"s mamma) <"+v(m,'contact_email_mum')+">"
             if v(m,'contact_email_dad') != "" and v(m,'contact_email_dad') != v(m,'email'):
-                elist.append([v(m,'contact_fathers_name') +" ("+namn+"s pappa)","<"+v(m,'contact_email_dad')+">"])
+                elista += v(m,'contact_fathers_name')+" ("+namn+"s pappa) <"+v(m,'contact_email_dad')+">"
             if v(m,'contact_alt_email') != "":
-                elist.append([namn+" (Extra)","<"+v(m,'contact_alt_email')+">"])
-        f = open("/home/hakan/Temp/listor/"+avd+".txt","w")
-        for l in elist:
-            s = l[0]+"  "+l[1]+";\n"
-            f.write(s)
-        f.close()
-        pass
-            
+                elista += namn+" (Extra) <"+v(m,'contact_alt_email')+">"
+        save_file(avd+".txt",elista)
+             
 
 def kontaktlista(memdata):
     pass
