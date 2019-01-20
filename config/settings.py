054f3ed1006b7af59d032e7e3a67935ef78266b2
Zyberit <hakan@violaberg.nu>
2019-01-20 21:10:17 +0100

    Updated requriements and settings

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 HOST_NAME = os.getenv('HOST_NAME','')
 # ALLOWED_HOSTS = ['*']
